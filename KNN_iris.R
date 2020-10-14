52d7540f4e1a0fbf380b30c905b2489337f92b25
KangChungLin <66732309+KangChungLin@users.noreply.github.com>
2020-10-14 14:49:51 +0800

    add KNN files

---
+++ KNN_iris.R
@@ -0,0 +1,22 @@
+library(caret)
+
+# Create index to split based on labels  
+index <- createDataPartition(iris$Species, p=0.8, list=FALSE)
+
+# Subset training set with index
+iris.training <- iris[index,]
+
+# Subset test set with index
+iris.test <- iris[-index,]
+
+# Train a model
+model_knn <- train(iris.training[, 1:4], iris.training[, 5], method='knn')
+
+# Predict the labels of the test set
+predictions<-predict.train(object=model_knn,iris.test[,1:4], type="raw")
+
+# Evaluate the predictions
+table(predictions)
+
+# Confusion matrix 
+confusionMatrix(predictions,iris.test[,5])
